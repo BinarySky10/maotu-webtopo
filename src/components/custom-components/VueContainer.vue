1b620fca14a5865e595b1ee936b8a88db3189102
binarysky <1670426403@qq.com>
2026-07-24 11:49:46 +0800

    1149

---
+++ src/components/custom-components/VueContainer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="box"></div>
+</template>
+<script setup lang="ts">
+import { ElDescriptions, ElDescriptionsItem } from 'element-plus';
+import type { PropType } from 'vue';
+const props = defineProps({});
+</script>
+<style scoped>
+.box {
+  width: 100%;
+  height: 100%;
+  background: #f5f5f7;
+  border: 1px solid #222;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
