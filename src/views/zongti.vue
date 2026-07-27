aebe9ec7eff21d261581bcde82c1107add2ede41
binarysky <1670426403@qq.com>
2026-07-27 16:17:33 +0800

    总体页面应用

--- src/views/zongti.vue
+++ src/views/zongti.vue
@@ -9,6 +9,10 @@
     <!--  -->
     <KvList :datalist="datalist"></KvList>
   </TeleportContainer>
+  <TeleportContainer :targetAId="'VueContainer-B8llECJ6Mm'" v-if="loadJson">
+    <!--  -->
+    <KvList :datalist="datalist2"></KvList>
+  </TeleportContainer>
 </template>
 <script setup lang="ts">
 import MtPreview from '@/components/mt-preview/index.vue';
@@ -40,6 +44,28 @@ const datalist = [
     label: '进水浊度',
     value: '000.00',
     unit: 'NTU'
+  }
+];
+const datalist2 = [
+  {
+    label: '总进水流量累计',
+    value: '12417067',
+    unit: 'm³'
+  },
+  {
+    label: '总进水流量瞬时',
+    value: '0000',
+    unit: 'm3/h'
+  },
+  {
+    label: '进水浊度',
+    value: '000.00',
+    unit: 'NTU'
+  },
+  {
+    label: '余氟',
+    value: '0.00',
+    unit: 'mg/h'
   }
 ];
 const loadJson = ref(false);
