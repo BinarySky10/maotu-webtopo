6a8b22345a1889cfe0dfa7c39b656f1d6bca4a11
binarysky <1670426403@qq.com>
2026-07-29 20:14:54 +0800

    数据组件

--- src/views/WaterGongshuibengzhanTele.vue
+++ src/views/WaterGongshuibengzhanTele.vue
@@ -1,24 +1,123 @@
 <template>
   <TeleportContainer :targetAId="'VueContainer-CsjxrpmY4l'">
     <!--  -->
-    <p>我是数据框1</p>
+    <!-- <p>我是数据框1</p> -->
+    <DataDisplay1 :datas="datas"></DataDisplay1>
   </TeleportContainer>
   <TeleportContainer :targetAId="'VueContainer-UuuTsweNX1'">
     <!--  -->
-    <p>我是数据框2</p>
+    <DataDisplay1 :datas="datas2"></DataDisplay1>
   </TeleportContainer>
   <TeleportContainer :targetAId="'VueContainer-PdMslzlaLE'">
     <!--  -->
-    <p>我是数据框3</p>
+    <DataDisplay1 :datas="datas3" :type="'white'"></DataDisplay1>
   </TeleportContainer>
 </template>
 <script lang="ts" setup>
 import { onMounted, ref, computed, nextTick } from 'vue';
 import { ElMessage } from 'element-plus';
 import TeleportContainer from '@/components/mt-preview/TeleportContainer.vue';
+import DataDisplay1 from './components/common/DataDisplay1.vue';
 const shujukuangs = [
   'VueContainer-CsjxrpmY4l',
   'VueContainer-UuuTsweNX1',
   'VueContainer-PdMslzlaLE'
 ];
+const datas = [
+  {
+    label: '1#供水泵手动频率设定：',
+    value: '11',
+    unit: 'Hz'
+  },
+  {
+    label: '2#供水泵手动频率设定：',
+    value: '22',
+    unit: 'Hz'
+  },
+  {
+    label: '3#供水泵手动频率设定：',
+    value: '00',
+    unit: 'Hz'
+  },
+  {
+    label: '4#供水泵手动频率设定：',
+    value: '00',
+    unit: 'Hz'
+  },
+  {
+    label: '5#供水泵手动频率设定：',
+    value: '00',
+    unit: 'Hz'
+  }
+];
+const datas2 = [
+  {
+    label: '吸水井保护低液位设定：',
+    value: '0.00',
+    unit: '米'
+  },
+  {
+    label: '吸水井恢复液位设定：',
+    value: '0.00',
+    unit: '米'
+  },
+  {
+    label: '报警压力设定：',
+    value: '22',
+    unit: 'MPa'
+  },
+  {
+    label: '恢复压力设定：',
+    value: '22',
+    unit: 'MPa'
+  },
+  {
+    label: '4#供水休眠频率设定：',
+    value: '00',
+    unit: 'Hz'
+  },
+  {
+    label: '4#供水唤醒压力设定：',
+    value: '00',
+    unit: 'Hz'
+  },
+  {
+    label: '休眠延时设定：',
+    value: '0000',
+    unit: 's'
+  },
+  {
+    label: '4#供水系统恒压设定：',
+    value: '0.00',
+    unit: 'Mpa'
+  }
+];
+const datas3 = [
+  {
+    label: '5#供水泵累计运行时间：',
+    value: '000000',
+    unit: '小时'
+  },
+  {
+    label: '4#供水泵累计运行时间：',
+    value: '000000',
+    unit: '小时'
+  },
+  {
+    label: '3#供水泵累计运行时间：',
+    value: '000000',
+    unit: '小时'
+  },
+  {
+    label: '2#供水泵累计运行时间：',
+    value: '000000',
+    unit: '小时'
+  },
+  {
+    label: '1#供水泵累计运行时间：',
+    value: '000000',
+    unit: '小时'
+  }
+];
 </script>
+<style scoped lang="less"></style>
---
+++ src/views/components/common/DataDisplay1.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="box-container">
+    <div class="box" v-for="(item, index) in props.datas" :key="index">
+      <div class="left">{{ item.label }}</div>
+      <div class="data right">
+        <div class="value" :class="props.type == 'black' ? 'black' : 'white'">{{ item.value }}</div>
+        <div class="unit">{{ item.unit }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'black'
+  },
+  datas: {
+    type: Array,
+    default: () => [
+      {
+        label: '1#供水泵手动频率设定：',
+        value: '00',
+        unit: 'Hz'
+      },
+      {
+        label: '2#供水泵手动频率设定：',
+        value: '00',
+        unit: 'Hz'
+      },
+      {
+        label: '3#供水泵手动频率设定：',
+        value: '00',
+        unit: 'Hz'
+      },
+      {
+        label: '4#供水泵手动频率设定：',
+        value: '00',
+        unit: 'Hz'
+      },
+      {
+        label: '5#供水泵手动频率设定：',
+        value: '00',
+        unit: 'Hz'
+      }
+    ]
+  }
+});
+</script>
+<style scoped lang="less">
+.box-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+
+  .box {
+    display: flex;
+    margin: 6px 15px;
+    border-radius: 4px;
+    font-weight: 500;
+    font-size: 14px;
+    background: #ddd;
+    .left {
+      flex: 3;
+      padding: 5px;
+    }
+    .right {
+      flex: 2;
+      padding: 5px;
+    }
+    .data {
+      display: flex;
+      justify-content: space-around;
+      .value {
+        width: 50%;
+        text-align: center;
+      }
+      .black {
+        background: #0f0a09;
+        color: #00c300;
+      }
+      .white {
+        background: #fff;
+        color: #000;
+      }
+      .unit {
+        color: #222fdd;
+      }
+    }
+  }
+}
+</style>
