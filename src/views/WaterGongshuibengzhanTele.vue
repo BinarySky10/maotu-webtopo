9f6e3c3785fc7c5c3a4ac6ebeb8098bedff02860
binarysky <1670426403@qq.com>
2026-07-29 18:08:27 +0800

    0729暂存

---
+++ src/views/WaterGongshuibengzhanTele.vue
@@ -0,0 +1,24 @@
+<template>
+  <TeleportContainer :targetAId="'VueContainer-CsjxrpmY4l'">
+    <!--  -->
+    <p>我是数据框1</p>
+  </TeleportContainer>
+  <TeleportContainer :targetAId="'VueContainer-UuuTsweNX1'">
+    <!--  -->
+    <p>我是数据框2</p>
+  </TeleportContainer>
+  <TeleportContainer :targetAId="'VueContainer-PdMslzlaLE'">
+    <!--  -->
+    <p>我是数据框3</p>
+  </TeleportContainer>
+</template>
+<script lang="ts" setup>
+import { onMounted, ref, computed, nextTick } from 'vue';
+import { ElMessage } from 'element-plus';
+import TeleportContainer from '@/components/mt-preview/TeleportContainer.vue';
+const shujukuangs = [
+  'VueContainer-CsjxrpmY4l',
+  'VueContainer-UuuTsweNX1',
+  'VueContainer-PdMslzlaLE'
+];
+</script>
