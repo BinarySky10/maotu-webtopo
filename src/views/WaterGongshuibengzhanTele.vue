ce9d98edfc17c7df26929970d3149efeb1ee240f
binarysky <1670426403@qq.com>
2026-07-29 20:55:29 +0800

    更新数据

--- src/views/WaterGongshuibengzhanTele.vue
+++ src/views/WaterGongshuibengzhanTele.vue
@@ -12,12 +12,36 @@
     <!--  -->
     <DataDisplay1 :datas="datas3" :type="'white'"></DataDisplay1>
   </TeleportContainer>
+
+  <TeleportContainer :targetAId="item.targetId" v-for="(item, index) in yeweiState" :key="index">
+    <KvOne :data="item.data"></KvOne>
+  </TeleportContainer>
+  <!-- 压力计 -->
+  <TeleportContainer :targetAId="item.targetId" v-for="(item, index) in yaliState" :key="index">
+    <KvOne :data="item.data"></KvOne>
+  </TeleportContainer>
+
+  <!-- 频率 -->
+  <TeleportContainer :targetAId="item.targetId" v-for="(item, index) in pinlvState" :key="index">
+    <KvOne :data="item.data"></KvOne>
+  </TeleportContainer>
+  <!-- 按钮 -->
+  <TeleportContainer :targetAId="item.targetId" v-for="item in anniuState">
+    <ScButton :type="item.data.type" @onClick="onClick(item.targetId)"></ScButton>
+  </TeleportContainer>
 </template>
 <script lang="ts" setup>
 import { onMounted, ref, computed, nextTick } from 'vue';
 import { ElMessage } from 'element-plus';
 import TeleportContainer from '@/components/mt-preview/TeleportContainer.vue';
 import DataDisplay1 from './components/common/DataDisplay1.vue';
+import KvList from './components/zongti/KvList.vue';
+import KvOne from './components/zongti/KvOne.vue';
+import ScButton from './components/zongti/ScButton.vue';
+const onClick = (vueContainerId: string) => {
+  console.log('click', vueContainerId);
+  ElMessage.success(`获取到了id:${vueContainerId}`);
+};
 const shujukuangs = [
   'VueContainer-CsjxrpmY4l',
   'VueContainer-UuuTsweNX1',
@@ -119,5 +143,65 @@ const datas3 = [
     unit: '小时'
   }
 ];
+const yeweis = ['VueContainer-8w71wzVa1e'];
+const yeweiState = yeweis.map((item) => {
+  return {
+    targetId: item,
+    data: {
+      label: '液位',
+      value: '3.73',
+      unit: 'M'
+    }
+  };
+});
+const pinlvs = [
+  'VueContainer-Ff7hmkAq7H',
+  'VueContainer-GHjr55NaLR',
+  'VueContainer-bBFR0SVCRi',
+  'VueContainer-e7TPOVQg6Y',
+  'VueContainer-e6SX2Ytj1C'
+];
+const pinlvState = pinlvs.map((item) => {
+  return {
+    targetId: item,
+    data: {
+      label: '频率',
+      value: '0.00',
+      unit: 'Hz'
+    }
+  };
+});
+const yalis = [
+  'VueContainer-64rSpkqZDq',
+  'VueContainer-k4tX8WSlLU',
+  'VueContainer-Mph65uofFT',
+  'VueContainer-g2tb3PyJMy',
+  'VueContainer-e6SX2Ytj1C'
+];
+const yaliState = yalis.map((item) => {
+  return {
+    targetId: item,
+    data: {
+      label: '压力',
+      value: '0.00',
+      unit: 'MPa'
+    }
+  };
+});
+const annius = [
+  'VueContainer-r3zQxrRpkk',
+  'VueContainer-pM8J2pRIF6',
+  'VueContainer-3gT2l0ikfH',
+  'VueContainer-UspIjwRcWt',
+  'VueContainer-KZjmkO8qxx'
+];
+const anniuState = annius.map((item) => {
+  return {
+    targetId: item,
+    data: {
+      type: Math.random() > 0.5 ? 'danger' : 'success'
+    }
+  };
+});
 </script>
 <style scoped lang="less"></style>
